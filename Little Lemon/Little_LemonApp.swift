c53d586fdf6f2f5548836ee4bbadc4540fa826bc
Muna Abdelwahab <muna.said9875@gmail.com>
2025-05-20 17:24:05 +0300

    Initial Commit

---
+++ Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Muna Abdelwahab on 20/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
